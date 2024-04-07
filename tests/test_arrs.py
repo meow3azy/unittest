c846ddc8efaf6b34170882bed693dbec035648b3
Pavel Isaev <killroy1297@gmail.com>
2024-04-08 00:44:30 +0300

    versfinal v2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,21 +1,30 @@
-import unittest
-from utils import arrs
+import pytest
+from arrs import get, my_slice
 
+def test_get_existing_index():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 2) == 3
 
-class TestArrs(unittest.TestCase):
+def test_get_nonexisting_index_with_default():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 10, "default") == "default"
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
-        self.assertEqual(arrs.get([1, 2, 3], 3, "test"), "test")
-        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
-        self.assertEqual(arrs.get([1, 2, 3], -3, "test"), "test")
-        self.assertEqual(arrs.get([1, 2, 3], 2, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+def test_get_nonexisting_index_without_default():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 10) == None
+
+def test_my_slice_no_arguments():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array) == [1, 2, 3, 4, 5]
+
+def test_my_slice_with_start():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array, 2) == [3, 4, 5]
+
+def test_my_slice_with_end():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array, end=3) == [1, 2, 3]
+
+def test_my_slice_with_start_and_end():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array, 1, 4) == [2, 3, 4]
