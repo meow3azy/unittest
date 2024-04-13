0c0eb2e7057a31c619c72eae5ef3b5a3ea081238
meow3azy <120301699+meow3azy@users.noreply.github.com>
2024-04-13 14:59:24 +0300

    Test/issue2 (#6)
    * versfinal v2
    
    * versfinal v2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,30 +1,30 @@
 import pytest
 from arrs import get, my_slice
-
-def test_get_existing_index():
+def test_get_negative_index():
     array = [1, 2, 3, 4, 5]
-    assert get(array, 2) == 3
+    assert get(array, -1) == None
 
-def test_get_nonexisting_index_with_default():
+def test_get_noninteger_index():
     array = [1, 2, 3, 4, 5]
-    assert get(array, 10, "default") == "default"
+    with pytest.raises(TypeError):
+        get(array, 2.5)
 
-def test_get_nonexisting_index_without_default():
+def test_my_slice_negative_start():
     array = [1, 2, 3, 4, 5]
-    assert get(array, 10) == None
+    assert my_slice(array, -2) == [4, 5]
 
-def test_my_slice_no_arguments():
+def test_my_slice_negative_end():
     array = [1, 2, 3, 4, 5]
-    assert my_slice(array) == [1, 2, 3, 4, 5]
+    assert my_slice(array, end=-2) == [1, 2, 3]
 
-def test_my_slice_with_start():
+def test_my_slice_start_and_end_out_of_bounds():
     array = [1, 2, 3, 4, 5]
-    assert my_slice(array, 2) == [3, 4, 5]
+    assert my_slice(array, -10, 10) == [1, 2, 3, 4, 5]
 
-def test_my_slice_with_end():
-    array = [1, 2, 3, 4, 5]
-    assert my_slice(array, end=3) == [1, 2, 3]
+def test_my_slice_non_list_or_tuple_argument():
+    with pytest.raises(TypeError):
+        my_slice("not_an_iterable")
 
-def test_my_slice_with_start_and_end():
-    array = [1, 2, 3, 4, 5]
-    assert my_slice(array, 1, 4) == [2, 3, 4]
+def test_get_non_list_argument():
+    with pytest.raises(TypeError):
+        get("not_an_iterable", 2)
